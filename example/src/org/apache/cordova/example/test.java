7c2cdf0af3172f45cad502dbfc4c777ef35df678
Keith Allen Whitson Jr <keith_whitson@yahoo.com>
2013-06-18 22:42:13 +0200

    it worked

---
+++ example/src/org/apache/cordova/example/test.java
@@ -0,0 +1,13 @@
+package org.apache.cordova.example;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
